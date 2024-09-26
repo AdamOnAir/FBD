8a999d1b61a75a18a8eed4c7fe566f0c83a9c944
Adam Ellouze <elzadam11@gmail.com>
2024-09-26 07:32:11 +0100

    Update airlib.h

--- include/airlib.h
+++ include/airlib.h
@@ -1,76 +1,95 @@
-#ifndef AIRLIB_LIBRARY_H
-#define AIRLIB_LIBRARY_H
+/* Copyright (c) 2024 Adam Ellouze. All Rights Reserved. */
 
-// ------------------------------------------------
-// Some Basic Info
-// ------------------------------------------------
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "library.h"
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+// ------------------------------------
+// Error handling
+// ------------------------------------
 
-// -------------------------------------------------
+char *errorLog(char *text, unsigned int error, unsigned int warn, unsigned int info) {
+
+  // Color
+  if (error) printf(KRED);
+  if (warn) printf(KYEL);
+  if (info) printf(KBLU);
+
+  // Text
+  if (error) printf("error: ");
+  if (warn) printf("warn: ");
+  if (info) printf("info: ");
+  printf("%s", text);
+
+  printf(KNRM);
+}
+
+// ------------------------------------
 // Color and Strings
-// -------------------------------------------------
+// ------------------------------------
 
-void DisplayRedColor(char *txt);
-void DisplayGreenColor(char *txt);
-void DisplayYellowColor(char *txt);
-void DisplayBlueColor(char *txt);
-void DisplayMagentaColor(char *txt);
-void DisplayCyanColor(char *txt);
-void DisplayWhiteColor(char *txt);
-void DisplayTitle(char *txt, char *color);
-void DisplaySubTitle(char *txt, char *color);
-void DisplayBreakLine();
+void richText(char *text, char *color, unsigned int bold, unsigned int italic, unsigned int underline) {
+    printf("%s", color);
+    if (bold) printf("\033[1m");
+    if (italic) printf("\033[3m");
+    if (underline) printf("\033[4m");
+    printf("%s", text);
+    printf(KNRM);  // Reset to normal
+    printf("\n");
+}
 
-// ------------------------------------------------
+// ------------------------------------
 // Window
-// ------------------------------------------------
+// ------------------------------------
 
-void InitWindow();
-void CloseWindowNy();
-void CloseWindow();
-void SegFault();
-void clrscr();
+void segFault() {
+    int *p = NULL;
+    *p = 1;
+}
 
-// ------------------------------------------------
+void clrscr() {
+    printf(KCLS);  // This clears the screen
+}
+
+// ------------------------------------
 // Clock and Time
-// ------------------------------------------------
+// ------------------------------------
 
-void SysSleep(int ms);
-float GetGameTime();
+void sysSleep(int ms) {
+    clock_t goal = ms * CLOCKS_PER_SEC / 1000;
+    clock_t start = clock();
+    while (clock() < goal) {
+    }
+}
 
-// ------------------------------------------------
+float getGameTime() {
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("The time is %d:%d:%d\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+}
+
+float stopWatch() {
+    clock_t start = clock();
+    clock_t end = clock();
+    float seconds = (float) (end - start) / CLOCKS_PER_SEC;
+    return seconds;
+}
+
+// ------------------------------------
 // Drawing
-// ------------------------------------------------
+// ------------------------------------
 
-void DrawRedLine();
-void DrawGreenLine();
-void DrawYellowLine();
-void DrawBlueLine();
-void DrawMagentaLine();
-void DrawCyanLine();
-void DrawWhiteLine();
-void DrawRedPolygon(int sides);
-void DrawGreenPolygon(int sides);
-void DrawYellowPolygon(int sides);
-void DrawBluePolygon(int sides);
-void DrawMagentaPolygon(int sides);
-void DrawCyanPolygon(int sides);
-void DrawWhitePolygon(int sides);
-
-// ------------------------------------------------
-// Math
-// ------------------------------------------------
-
-float DEG2RAD(float deg);
-float RAD2DEG(float rad);
-float sqrt(float x);
-
-#endif //AIRLIB_LIBRARY_H
+void drawBox(int x, int y, int width, int height, char *color) {
+    printf("%s", color);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf(KNRM);  // Reset to normal
+}
